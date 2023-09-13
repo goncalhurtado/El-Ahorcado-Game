6a8f32dc581f283bc51f19fca233e7a8055a5d94
goncalhurtado <gonzalomilano@gmail.com>
2023-09-13 14:54:21 -0300

    added sass

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,16 @@ import React, { useEffect } from "react";
 const Player = ({ contador }) => {
   return (
     <>
-      <img src={`src/assets/img/${contador}.png`} alt="img" />
+      <img
+        src={`src/assets/img/${contador}.png`}
+        alt="img"
+        className="player-img"
+      />
+      {/* <img
+        src={`https://res.cloudinary.com/dhvgi2cmq/image/upload/v1694626625/ahorcadito-game-test/${contador}_ahorcadito-game.png`}
+        alt="img"
+        className="player-img"
+      /> */}
     </>
   );
 };
