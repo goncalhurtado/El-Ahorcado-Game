cab55ec365a6064bdda0456a969baf08a382b4ca
goncalhurtado <gonzalomilano@gmail.com>
2023-09-13 17:50:16 -0300

    fixed problem with accents

--- src/components/WordInput.jsx
+++ src/components/WordInput.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const WordInput = ({ word }) => {
+  console.log(word);
   return (
     <input
       type="text"
-      defaultValue={word.guessed === true ? word.letter : ""}
+      defaultValue={word.guessed === true ? word.realLetter : ""}
       readOnly
       className={word.guessed === true ? "word-input-guessed" : "word-input"}
     />
--- src/helpers/splitWord.js
+++ src/helpers/splitWord.js
@@ -8,9 +8,10 @@ export let splitWord = (word) => {
         }
 
         let letter = wordSplit[i];
-        let isVocal = ["a", "e", "i", "o", "u"].includes(letter);
+        let isVocal = ["a", "á", "e", "é", "i", "í", "o", "ó", "u", "ú"].includes(letter);
         let letterObj = {
-            letter: letter,
+            realLetter: letter,
+            letter: quitarAcentos(letter),
             key: Math.floor(Math.random() * 9000) + 1000,
             guessed: isVocal,
             vocal: isVocal,
@@ -20,3 +21,15 @@ export let splitWord = (word) => {
 
     return wordObj;
 };
+
+
+function quitarAcentos(letra) {
+    const acentos = {
+        'á': 'a',
+        'é': 'e',
+        'í': 'i',
+        'ó': 'o',
+        'ú': 'u',
+    };
+    return acentos[letra] || letra;
+}
